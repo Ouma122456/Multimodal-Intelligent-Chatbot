9f934eee931bfb84ce58ec73c3d99a33c0a88786
Oumayma abassi <154348975+Ouma122456@users.noreply.github.com>
2025-11-12 22:44:15 +0100

    Delete script directory

--- script/test_model.py
+++
@@ -1,6 +0,0 @@
-from llama_cpp import Llama
-
-llm = Llama(model_path=r"C:\Users\abass\llama.cpp\models\llama-pro-8b-instruct.Q4_K_M.gguf")
-
-output = llm("Hello, how are you?", max_tokens=100)
-print(output["choices"][0]["text"])
